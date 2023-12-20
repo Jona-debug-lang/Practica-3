23045f682fc5e750364900957de5be08e9c8c663
Madelacu <mdelacuadra01@gmail.com>
2023-12-20 17:42:03 +0100

    Documentos faltantes, search bar, diseño load more

--- public/app.js
+++ public/app.js
@@ -7,13 +7,32 @@ async function loadMore(){
     const from = (loadMoreRequests+1) * NUM_RESULTS;
     const to = from + NUM_RESULTS;
 
-    const response = await fetch(`/corrientes?from=${from}&to=${to}`);
+    const response = await fetch(`/corriente?from=${from}&to=${to}`); //EL de corriente es del html
 
-    const newCorrientes = await response.text();
+    const newPosts = await response.text();
   
-    const corrientesDiv = document.getElementById("corrientes");
+    const postsDiv = document.getElementById("corriente"); //El de corriente es del html a renderizar
 
-    corrientesDiv.innerHTML += newCorrientes;
+    postsDiv.innerHTML += newPosts;
 
     loadMoreRequests++;
+}
+
+function search() {
+    let input = document.getElementById('searchbar').value //Obtiene la entrada del buscador
+    input = input.toLowerCase(); //Lo convierte en minúsculas
+    let x = document.getElementsByClassName('subtitle'); //Obtiene todos los elementos que tienen la clase subtitle
+    
+    //Inicia un bucle que itera sobre todos los elementos "subtitle"
+    for (i = 0; i < x.length; i++) { 
+        let current_title = x[i]; //Obtiene el elemento que se está iterando
+        let relatedPost = current_title.closest('.post-item'); //Encuentra el contenedor del elemento más cercano al título que se está iterando
+
+        //Revisa si el texto del título no incluye el texto de la búsqueda
+        if (!current_title.innerHTML.toLowerCase().includes(input)) {
+            relatedPost.style.display = "none"; //None para ocultarlo
+        } else {
+            relatedPost.style.display = "block"; //Block para que se muestre como bloque
+        }
+    }
 }
--- src/router.js
+++ src/router.js
@@ -1,28 +1,74 @@
 import express from 'express';
-import { getCorrientes } from './corrientes.js';
+import * as service from './service.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
 
-    const corrientes = getCorrientes(0,3);
+    const posts = service.getPosts(0,3);
 
-    res.render('index', { 
-        corrientes: corrientes
+    res.render('index', {
+        posts: posts
     });
 });
 
-router.get('/corrientes', (req, res) => {
+router.get('/corriente', (req, res) => { //Corriente es el HTML
 
     const from = parseInt(req.query.from);
     const to = parseInt(req.query.to);
 
-    const corrientes = getCorrientes(from,to);
+    const posts = service.getPosts(from,to);
 
-    res.render('corrientes', { 
-        corrientes: corrientes 
+    res.render('corriente', {    //Corriente es el HTML
+        posts: posts
     });
 });
 
+// Definición de una ruta GET para mostrar un post específico utilizando Express Router
+router.get('/post/:id', (req, res) => {
+    let post = service.getPost(req.params.id);
+    res.render('show_post', { post });
+});
+
+//Ruta para actualizar una publicación con los subelementos
+router.post('/post/:id/update', (req, res) => {
+    const { image, title, date1, date2, edad1, edad2, descripcion, subelemento } = req.body;
+    const postId = req.params.id;
+    const subelementoArray = subelemento ? [subelemento] : [];
+    service.updatePost(postId, { image, title, date1, date2, edad1, edad2, descripcion, subelemento: subelementoArray[0] });
+    let post = service.getPost(req.params.id);
+    res.render('show_post', { post });
+});
+
+// Definición de una ruta GET para eliminar un post específico utilizando Express Router
+router.get('/post/:id/delete', (req, res) => {
+    service.deletePost(req.params.id);
+    res.redirect('/');
+});
+
+// Definición de una ruta GET para mostrar el formulario de edición de un post específico utilizando Express Router
+router.get('/post/:id/edit', (req, res) => {
+    let post = service.getPost(req.params.id);
+    res.render('edit_post', { post });
+});
+
+// Definición de una ruta POST para procesar la edición de un post específico utilizando Express Router
+router.post('/post/:id/edit/edits', (req, res) => {
+    let { image, title, date1, date2, edad1, edad2, descripcion, subelemento } = req.body;
+    let id = req.params.id;
+    let referer = req.get('referer');
+    console.log('Referer:', referer);
+    let updatedReferer = referer.replace('/edit', '');
+    let existingPost = service.getPost(id);
+    existingPost.image = image;
+    existingPost.title = title;
+    existingPost.date1 = date1;
+    existingPost.date2 = date2;
+    existingPost.edad1 = edad1;
+    existingPost.edad2 = edad2;
+    existingPost.descripcion = descripcion;
+    service.edit_post(existingPost, id);
+    res.redirect(updatedReferer);
+});
 
 export default router;
