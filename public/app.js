b3efc1b779bca2ad158c251cae06e3943e167ec7
Jona-debug-lang <a01275322@tec.mx>
2023-12-20 17:50:57 +0100

    cambio de modal

--- public/app.js
+++ public/app.js
@@ -29,7 +29,7 @@ function search() {
         let relatedPost = current_title.closest('.post-item'); //Encuentra el contenedor del elemento más cercano al título que se está iterando
 
         //Revisa si el texto del título no incluye el texto de la búsqueda
-        if (!current_title.innerHTML.toLowerCase().includes(input)) {
+        if (!current_title.innerHTML.toLowerCase().startsWith(input)) {
             relatedPost.style.display = "none"; //None para ocultarlo
         } else {
             relatedPost.style.display = "block"; //Block para que se muestre como bloque
